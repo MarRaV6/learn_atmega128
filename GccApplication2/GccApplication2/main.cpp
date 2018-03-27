e0e7e35dd2961a3696331512fe98fde9e3ef5ffa
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2018-03-27 13:07:57 +0300

    +аппаратный ШИМ на PC5, убран старый код таймера

--- GccApplication2/GccApplication2/main.cpp
+++ GccApplication2/GccApplication2/main.cpp
@@ -22,6 +22,9 @@
 // ???
 #define max_n 11
 
+#define TIMER_TOP 0x3FF
+#define PWM_OCR OCR1A
+
 unsigned int read_adc(unsigned char adc_input);
 void adc_init(uint8_t);
 
@@ -110,12 +113,7 @@ void lcd_array( char x, char y, const char *str )
 char upper_line[17];        //Массив для верхней строки LCD дисплея
 char lower_line[17];        //Массив для нижней строки LCD дисплея
 
-DependResist dependResist[(max_n+1)]; // создаем калибровочную таблицу на max_n значений
-
-uint64_t time = 0;  // наше собственное время, 64бит должно хватить на пару миллиардов лет
-
-uint8_t pwm_load = 0;  // мера скважности
-uint16_t pwm_period = 100;  // количество отсчетов, которые формируют цикл ШИМ
+DependResist dependResist[(max_n+1)]; // создаем калибровочную таблицу на max_n
 
 // enum Screen {
 //	screenTemp = 2,
@@ -128,61 +126,37 @@ void Timer_Init()
 {
     // Timer/Counter 1 initialization
     // Clock source: System Clock
-    // Clock value: 9,766 kHz
-    // Mode: CTC top=OCR1A
-    // OC1A output: Disconnected
+    // Clock value: 7,816 kHz
+    // Mode: Fast PWM top=0x03FF
+    // OC1A output: Non-Inverted PWM
     // OC1B output: Disconnected
     // OC1C output: Disconnected
     // Noise Canceler: Off
     // Input Capture on Falling Edge
-    // Timer Period: 0,1024 ms
+    // Timer Period: 0,13101 s
+    // Output Pulse(s):
+    // OC1A Period: 0,13101 s Width: 0 us
     // Timer1 Overflow Interrupt: Off
     // Input Capture Interrupt: Off
     // Compare A Match Interrupt: Off
     // Compare B Match Interrupt: Off
     // Compare C Match Interrupt: Off
-    TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<COM1C1) | (0<<COM1C0) | (1<<WGM11) | (0<<WGM10);
+    TCCR1A=(1<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<COM1C1) | (0<<COM1C0) | (1<<WGM11) | (1<<WGM10);
     TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (1<<WGM12) | (1<<CS12) | (0<<CS11) | (1<<CS10);
     TCNT1H=0x00;
     TCNT1L=0x00;
     ICR1H=0x00;
-    ICR1L=0x00;
+    ICR1L=0x61;
     OCR1AH=0x00;
     OCR1AL=0x00;
     OCR1BH=0x00;
     OCR1BL=0x00;
     OCR1CH=0x00;
     OCR1CL=0x00;
-    
+
+    // Timer(s)/Counter(s) Interrupt(s) initialization
     TIMSK=(0<<OCIE2) | (0<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<TOIE1) | (0<<OCIE0) | (0<<TOIE0);
     ETIMSK=(0<<TICIE3) | (0<<OCIE3A) | (0<<OCIE3B) | (0<<TOIE3) | (0<<OCIE3C) | (0<<OCIE1C);
-    
-    EICRA=(0<<ISC31) | (0<<ISC30) | (0<<ISC21) | (0<<ISC20) | (0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (0<<ISC00);
-    EICRB=(0<<ISC71) | (0<<ISC70) | (0<<ISC61) | (0<<ISC60) | (0<<ISC51) | (0<<ISC50) | (0<<ISC41) | (0<<ISC40);
-    EIMSK=(0<<INT7) | (0<<INT6) | (0<<INT5) | (0<<INT4) | (0<<INT3) | (0<<INT2) | (0<<INT1) | (0<<INT0);
-
-    
-    TIMSK |= (1<<TOIE1); // разрешаем прерывание по переполнению таймера
-    TCNT1 = 64456;        // выставляем начальное значение TCNT1
-    
-    // USART0 initialization
-    // USART0 disabled
-    UCSR0B=(0<<RXCIE0) | (0<<TXCIE0) | (0<<UDRIE0) | (0<<RXEN0) | (0<<TXEN0) | (0<<UCSZ02) | (0<<RXB80) | (0<<TXB80);
-};
-
-// Timer1 overflow interrupt service routine
-//interrupt [TIM1_OVF] void timer1_ovf_isr(void)
-ISR(TIMER1_OVF_vect)
-{
-    // просигналим портом, что наш таймер вообще работает
-    if (PORTB == 1) {
-        PORTB = 0;
-    }
-    else {
-        PORTB = 1;
-    }
-    
-    time++;  // инкрементировать наш собственный счетчик времени, по которому из главного цикла делать ШИМ
 };
 
 int main(void) {
@@ -209,6 +183,7 @@ int main(void) {
     char screen_for_temp = 'T';
     char sreen_for_ust = 'F';
     uint8_t ust = 30;
+    uint8_t pwm_load = 0;  // мера скважности
     
     // enum Screen screen = screenTemp;
     
@@ -276,20 +251,10 @@ int main(void) {
         lcd_array(1,0,upper_line);  //Вывод массива верхней строки на дисплей
         lcd_array(0,1,lower_line);  //Вывод массива нижней строки на дисплей
 
-        pwm_load = 50;  // todo с помощью PID регулятора определить скважность (pwm_load)
+        pwm_load = 0.5;  // todo с помощью PID регулятора определить скважность (pwm_load)
+        PWM_OCR = (uint16_t)(pwm_load * TIMER_TOP);  // изменим широту импулься PWM
 
-        // наш собственный пролетарский ШИМ v0, потому что аппаратный для слабаков
-        if (time % pwm_period * 100 / pwm_period <= pwm_load) {
-            PORTC = 1;
-        } else {
-            PORTC = 0;
-};
-
-        _delay_ms(100);  // todo wtf
-
-        // OCR1A = (uint16_t)(0.2 * 0x3FF);
-        // OCR1AL = 0x3FF;
-        OCR1A = 64000;  // значение таймера при котором сработает прерывание
+        // _delay_ms(100);  // todo wtf
     }
 }
 
